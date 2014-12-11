842591f3112732fc70d5fabec611a7a679d3edaa
onpon4 <onpon4@riseup.net>
2014-12-11 10:33:38 -0500

    Added a test of moving walls to the physics example.

--- examples/physics.py
+++ examples/physics.py
@@ -68,6 +68,51 @@ class Player(physics.Collider):
             self.yvelocity = min(2, self.yvelocity)
 
 
+class Square(physics.Solid, physics.MobileWall):
+
+    def event_step(self, time_passed, delta_mult):
+        self.xvelocity = (sge.keyboard.get_pressed("d") -
+                          sge.keyboard.get_pressed("a")) * 4
+        self.yvelocity = (sge.keyboard.get_pressed("s") -
+                          sge.keyboard.get_pressed("w")) * 4
+
+
+class DiamondTopLeft(physics.SlopeTopLeft, physics.MobileWall):
+
+    def event_step(self, time_passed, delta_mult):
+        self.xvelocity = (sge.keyboard.get_pressed("l") -
+                          sge.keyboard.get_pressed("j")) * 4
+        self.yvelocity = (sge.keyboard.get_pressed("k") -
+                          sge.keyboard.get_pressed("i")) * 4
+
+
+class DiamondTopRight(physics.SlopeTopRight, physics.MobileWall):
+
+    def event_step(self, time_passed, delta_mult):
+        self.xvelocity = (sge.keyboard.get_pressed("l") -
+                          sge.keyboard.get_pressed("j")) * 4
+        self.yvelocity = (sge.keyboard.get_pressed("k") -
+                          sge.keyboard.get_pressed("i")) * 4
+
+
+class DiamondBottomLeft(physics.SlopeBottomLeft, physics.MobileWall):
+
+    def event_step(self, time_passed, delta_mult):
+        self.xvelocity = (sge.keyboard.get_pressed("l") -
+                          sge.keyboard.get_pressed("j")) * 4
+        self.yvelocity = (sge.keyboard.get_pressed("k") -
+                          sge.keyboard.get_pressed("i")) * 4
+
+
+class DiamondBottomRight(physics.SlopeBottomRight, physics.MobileWall):
+
+    def event_step(self, time_passed, delta_mult):
+        self.xvelocity = (sge.keyboard.get_pressed("l") -
+                          sge.keyboard.get_pressed("j")) * 4
+        self.yvelocity = (sge.keyboard.get_pressed("k") -
+                          sge.keyboard.get_pressed("i")) * 4
+
+
 def main():
     # Create Game object
     Game(800, 600)
@@ -89,6 +134,20 @@ def main():
     slope4_sprite = sge.Sprite(width=16, height=16)
     slope4_sprite.draw_polygon([(16, 16), (16, 0), (0, 0)],
                                fill=sge.Color("white"))
+    sqwall_sprite = sge.Sprite(width=32, height=32)
+    sqwall_sprite.draw_rectangle(0, 0, 32, 32, fill=sge.Color("red"))
+    diamond_tl_sprite = sge.Sprite(width=16, height=16)
+    diamond_tl_sprite.draw_polygon([(0, 16), (16, 16), (16, 0)],
+                                   fill=sge.Color("red"))
+    diamond_tr_sprite = sge.Sprite(width=16, height=16)
+    diamond_tr_sprite.draw_polygon([(0, 0), (0, 16), (16, 16)],
+                                   fill=sge.Color("red"))
+    diamond_bl_sprite = sge.Sprite(width=16, height=16)
+    diamond_bl_sprite.draw_polygon([(0, 0), (16, 0), (16, 16)],
+                                   fill=sge.Color("red"))
+    diamond_br_sprite = sge.Sprite(width=16, height=16)
+    diamond_br_sprite.draw_polygon([(0, 0), (0, 16), (16, 0)],
+                                   fill=sge.Color("red"))
 
     # Load backgrounds
     background = sge.Background([], sge.Color("black"))
@@ -127,10 +186,16 @@ def main():
     slope4_1 = physics.SlopeBottomLeft(700, 100, 0, sprite=slope4_sprite)
     slope4_2 = physics.SlopeBottomLeft(716, 116, 0, sprite=slope4_sprite)
     slope4_3 = physics.SlopeBottomLeft(684, 84, 0, sprite=slope4_sprite)
+    square = Square(10, 300, 0, sprite=sqwall_sprite)
+    diamond_tl = DiamondTopLeft(700, 240, 0, sprite=diamond_tl_sprite)
+    diamond_tr = DiamondTopRight(716, 240, 0, sprite=diamond_tr_sprite)
+    diamond_bl = DiamondBottomLeft(700, 256, 0, sprite=diamond_bl_sprite)
+    diamond_br = DiamondBottomRight(716, 256, 0, sprite=diamond_br_sprite)
     objects = [player, bottom_1, bottom_2, bottom_3, top_1, top_2, top_3,
                left_1, left_2, left_3, right_1, right_2, right_3,
                slope1_1, slope1_2, slope1_3, slope2_1, slope2_2, slope2_3,
-               slope3_1, slope3_2, slope3_3, slope4_1, slope4_2, slope4_3]
+               slope3_1, slope3_2, slope3_3, slope4_1, slope4_2, slope4_3,
+               square, diamond_tl, diamond_tr, diamond_bl, diamond_br]
 
     # Create room
     sge.game.start_room = sge.Room(objects, background=background)
