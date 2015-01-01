857535eb227a4959a5a1d0fc6d0fea6404c025bf
onpon4 <onpon4@riseup.net>
2014-12-31 19:31:42 -0500

    xSGE 0.5 release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ specification.
 """.strip()
 
 setup(name="xsge",
-      version="0.5a0",
+      version="0.5",
       description="Extensions for the SGE",
       long_description=long_description,
       author="Julian Marchant",
