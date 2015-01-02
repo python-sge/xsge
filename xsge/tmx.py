cb747d1cd8f8edb1467df3264892e46e43516ecb
onpon4 <onpon4@riseup.net>
2015-01-02 13:11:15 -0500

    Changed license of the platformer example to CC0.

--- xsge/tmx.py
+++ xsge/tmx.py
@@ -1,5 +1,5 @@
 # xSGE TMX Library
-# Copyright (c) 2014 Julian Marchant <onpon4@riseup.net>
+# Copyright (c) 2014, 2015 Julian Marchant <onpon4@riseup.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
