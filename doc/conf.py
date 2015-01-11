d305eb8f4f19b347c74a0cf5274b2763c7ccde82
onpon4 <onpon4@riseup.net>
2015-01-11 09:56:27 -0500

    Changed logo

--- doc/conf.py
+++ doc/conf.py
@@ -121,7 +121,7 @@ html_theme = 'nature'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "sge_logo_alpha_black.png"
+html_logo = "xsge_logo_alpha_black.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
