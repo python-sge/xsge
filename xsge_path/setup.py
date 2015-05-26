c7f3809f7ef2c132c7477a3d08a81517633868f0
onpon4 <onpon4@riseup.net>
2015-05-26 06:59:58 -0400

    xsge_path 0.9

--- xsge_path/setup.py
+++ xsge_path/setup.py
@@ -37,7 +37,7 @@ objects move in a certain way.
 """.strip()
 
 setup(name="xsge_path",
-      version="0.8",
+      version="0.9",
       description="xSGE Path",
       long_description=long_description,
       author="Julian Marchant",
