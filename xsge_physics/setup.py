b2e48c3e64d7d5d7941f04c1930fbbb0c6f51cf7
onpon4 <onpon4@riseup.net>
2015-08-24 09:02:36 -0400

    xsge_physics 0.9, xsge_tmx 0.9.1.

--- xsge_physics/setup.py
+++ xsge_physics/setup.py
@@ -38,7 +38,7 @@ for other types of games.
 """.strip()
 
 setup(name="xsge_physics",
-      version="0.9a0",
+      version="0.9",
       description="xSGE Physics Framework",
       long_description=long_description,
       author="Julian Marchant",
