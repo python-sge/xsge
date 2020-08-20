81846ecb5e80c40b05b2dbd4e9a7ac4d902964a2
Layla Marchant <diligentcircle@riseup.net>
2020-08-19 21:59:05 -0400

    Added supplemental files.

---
+++ xsge_tiled/doc-src/index.rst
@@ -0,0 +1,30 @@
+****************
+xSGE TMX Library
+****************
+
+.. This file has been dedicated to the public domain, to the extent
+   possible under applicable law, via CC0. See
+   http://creativecommons.org/publicdomain/zero/1.0/ for more
+   information. This file is offered as-is, without any warranty.
+
+.. contents::
+
+.. automodule:: xsge_tiled
+
+xsge_tiled Classes
+==================
+
+.. autoclass:: xsge_tiled.Decoration
+
+.. autoclass:: xsge_tiled.Rectangle
+
+.. autoclass:: xsge_tiled.Ellipse
+
+.. autoclass:: xsge_tiled.Polygon
+
+.. autoclass:: xsge_tiled.Polyline
+
+xsge_tiled Functions
+====================
+
+.. autofunction:: xsge_tiled.load
---
+++ xsge_tiled/setup.py
@@ -0,0 +1,45 @@
+# This file has been dedicated to the public domain, to the extent
+# possible under applicable law, via CC0. See
+# http://creativecommons.org/publicdomain/zero/1.0/ for more
+# information. This file is offered as-is, without any warranty.
+
+import sys
+from distutils.core import setup
+
+long_description = """
+xSGE is a collection of extensions for the SGE licensed under the GNU
+General Public License.  They are designed to give additional features
+to free/libre software games which aren't necessary, but are nice to
+have.
+
+xSGE extensions are not dependent on any particular SGE implementation.
+They should work with any implementation that follows the specification.
+
+This extension provides support for loading the JSON format of the
+`Tiled Map Editor <http://www.mapeditor.org/>`_.  This allows you to use
+Tiled to edit your game's world (e.g. levels), rather than building a
+level editor yourself.
+""".strip()
+
+setup(name="xsge_tiled",
+      version="0.1",
+      description="xSGE Tiled Library",
+      long_description=long_description,
+      author="Layla Marchant",
+      author_email="diligentcircle@riseup.net",
+      url="https://python-sge.github.io",
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: DFSG approved",
+                   "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Games/Entertainment",
+                   "Topic :: Software Development"],
+      license="GNU General Public License",
+      packages=["xsge_tiled"],
+      package_dir={"xsge_tiled": "xsge_tiled"},
+      package_data={"xsge_tiled": ["COPYING"]},
+      requires=["sge (>=1.0, <2.0)"],
+      provides=["xsge_tiled"],
+     )
